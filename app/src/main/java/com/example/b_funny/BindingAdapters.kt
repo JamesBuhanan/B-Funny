cf68af7c9395a790c63e4cb44f67019c1e0f047b
JamesBuhanan <JamesBuhanan@gmail.com>
2022-08-25 09:05:01 -0700

    Get thumbnail showing

--- app/src/main/java/com/example/b_funny/BindingAdapters.kt
+++ app/src/main/java/com/example/b_funny/BindingAdapters.kt
@@ -17,8 +17,12 @@
 
 package com.example.b_funny
 
+import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.b_funny.model.RedditPost
 import com.example.b_funny.overview.OverviewAdapter
 
@@ -31,18 +35,19 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<RedditPost>?) {
     adapter.submitList(data)
 }
 
-///**
-// * Uses the Glide library to load an image by URL into an [ImageView]
-// */
-//@BindingAdapter("imageUrl")
-//fun bindImage(imgView: ImageView, imgUrl: String?) {
-//    imgUrl?.let {
-//        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
-//        Glide.with(imgView.context)
-//                .load(imgUri)
-//                .apply(RequestOptions()
+/**
+ * Uses the Glide library to load an image by URL into an [ImageView]
+ */
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+                .load(imgUri)
+//                .apply(
+//                    RequestOptions()
 //                        .placeholder(R.drawable.loading_animation)
 //                        .error(R.drawable.ic_broken_image))
-//                .into(imgView)
-//    }
-//}
+                .into(imgView)
+    }
+}
