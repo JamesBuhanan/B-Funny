8e0c694219c70dd3fca11f4f84eca1a409b1213d
JamesBuhanan <JamesBuhanan@gmail.com>
2022-08-30 08:31:33 -0700

    Share url link with friends (and app color changes)

--- app/src/main/java/com/example/b_funny/detail/DetailFragment.kt
+++ app/src/main/java/com/example/b_funny/detail/DetailFragment.kt
@@ -1,10 +1,13 @@
 package com.example.b_funny.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
+import com.example.b_funny.R
 import com.example.b_funny.databinding.FragmentDetailBinding
 
 /**
@@ -20,7 +23,22 @@ class DetailFragment : Fragment() {
 
         val redditPost = DetailFragmentArgs.fromBundle(requireArguments()).selectedRedditPost
         binding.redditPost = redditPost
+        binding.btnShare.setOnClickListener {
+            share(redditPost.url!!)
+        }
 
         return binding.root
+    }
+
+    // Creating our Share Intent
+    private fun getShareIntent(url: String): Intent {
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(getString(R.string.sharing).format(url))
+            .setType("text/plain")
+            .intent
+    }
+
+    private fun share(url: String) {
+        startActivity(getShareIntent(url))
     }
 }
